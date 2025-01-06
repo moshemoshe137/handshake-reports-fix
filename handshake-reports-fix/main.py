36321cdd40984f3aefa5d2163a6c0f8780dbe0ac
Moshe Rubin <mosherubin137@gmail.com>
2025-01-06 15:59:40 -0600

    Add dotenv support for environment variables

--- handshake-reports-fix/main.py
+++ handshake-reports-fix/main.py
@@ -1,6 +1,23 @@
 """Read emails from Outlook."""
 
+import os
+
 import win32com.client
+from dotenv import load_dotenv
+
+
+def parse_env_vars() -> None:
+    """Parse configuration from environment variables and `.env` file."""
+    # Initialize the `python-dotenv` ".env" file, if present...
+    load_dotenv()
+    # and read the environment variables:
+
+    # 1. `chdir` to the network path specified by `HANDSHAKE_NETWORK_PATH`, if present.
+    if network_path := os.getenv("HANDSHAKE_NETWORK_PATH"):
+        # Change the current working directory to the network path specified by the
+        # variable `HANDSHAKE_NETWORK_PATH` in the .env file, if present.
+        os.chdir(network_path)
+    # 2. Handle other environment variables in the future?
 
 
 def load_emails() -> None:
@@ -24,4 +41,5 @@ def load_emails() -> None:
 
 
 if __name__ == "__main__":
+    parse_env_vars()
     load_emails()
