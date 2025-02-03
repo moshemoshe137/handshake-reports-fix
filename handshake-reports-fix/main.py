d3cb95f951bd2e59aefb37e71ade4d00c191fbf4
Moshe Rubin <mosherubin137@gmail.com>
2025-02-03 13:57:16 -0600

    Check inbox for Handshake reports
    and
      - Add `tqdm` for progress bars
      - Add type aliases for better type hints

--- handshake-reports-fix/main.py
+++ handshake-reports-fix/main.py
@@ -2,12 +2,25 @@
 
 import datetime
 import os
+from collections.abc import Generator
 from pathlib import Path
+from typing import TypeAlias
 
 import win32com.client
 from dotenv import load_dotenv
+from tqdm.auto import tqdm
 
 N_DAYS_KEPT = 100  # Number of days to keep emails before deleting them.
+
+HS_EMAIL = "handshake@notifications.joinhandshake.com"
+included_folders = ("Inbox", "Handshake-Reports")
+
+
+# Custom types for clearer type hints.
+OutlookNamespace: TypeAlias = win32com.client.CDispatch
+OutlookFolder: TypeAlias = win32com.client.CDispatch
+OutlookMailItem: TypeAlias = win32com.client.CDispatch
+OutlookItems: TypeAlias = win32com.client.CDispatch
 
 
 def parse_env_vars() -> None:
@@ -24,9 +37,25 @@ def parse_env_vars() -> None:
     # 2. Handle other environment variables in the future?
 
 
+def get_messages(
+    root_folder: OutlookFolder,
+    sort: bool = True,
+    include_dirs: tuple[str, ...] = included_folders,
+) -> Generator[OutlookMailItem, None, None]:
+    """Get messages from the specified folders in Outlook."""
+    for folder in root_folder.Folders:
+        if folder.Name in include_dirs:
+            messages: OutlookItems = folder.Items
+            if sort:
+                messages.Sort("[ReceivedTime]", True)
+
+            yield from tqdm(messages, desc=f'Scanning "{folder.Name}"')
+
+
 def load_emails() -> None:
     """Read emails from the "handshake-reports" folder in Outlook."""
     # Connect to Outlook
+    outlook: OutlookNamespace
     outlook = win32com.client.Dispatch("Outlook.Application").GetNamespace("MAPI")
 
     # Determine the Current User's email address
@@ -39,26 +68,22 @@ def load_emails() -> None:
         else outlook.CurrentUser.Address
     )
 
-    # Access the "Handshake-Reports" folder
     root_folder = outlook.Folders[users_email]
-    handshake_reports_folder = root_folder.Folders["Handshake-Reports"]
-
-    # Get the items (emails) in the folder
-    messages = handshake_reports_folder.Items
-    messages.Sort("[ReceivedTime]", True)  # Sort by received time (newest first)
 
     # Process the emails
-    for i, message in enumerate(messages):
-        # 1. Find unread messages with the right subject.
+    for i, message in enumerate(get_messages(root_folder)):
+        # 1. Skip messages that aren't relevant.
         if (
             "BILAKE" not in message.Subject  # If the subject doesn't contain "BILAKE"
-            or "error" in message.Subject.casefold()  # or if contains "error"
+            or "error" in message.Subject.casefold()  # or if it contains "error"
             or not message.UnRead  # or if it's already read
+            or message.SenderEmailAddress != HS_EMAIL  # or it's not from Handshake
         ):
-            # Skip this email! It's got the wrong subject or is already read.
+            # Skip this email! It's got the wrong subject, wrong sender, or is
+            # already read.
             continue
 
-        print(f"{i}. Subject: {message.Subject}, Received: {message.ReceivedTime}")
+        tqdm.write(f"{i}. Subject: {message.Subject}, Received: {message.ReceivedTime}")
 
         # 2. Download attachments.
         if message.Attachments.Count == 0:
@@ -69,7 +94,7 @@ def load_emails() -> None:
         for attachment in message.Attachments:
             file_name = Path(attachment.FileName).resolve()
             attachment.SaveAsFile(file_name)
-            print(f'Saved attachment {attachment.FileName} to "{file_name}"')
+            tqdm.write(f'Saved attachment {attachment.FileName} to "{file_name}"')
 
         # 3. Mark the email as read.
         message.UnRead = False
@@ -80,7 +105,7 @@ def load_emails() -> None:
         today = datetime.datetime.now(datetime.UTC)
         if today - message.ReceivedTime > datetime.timedelta(days=N_DAYS_KEPT):
             message.Delete()
-            print(f"Deleted email {message.Subject} ({i:,})")
+            tqdm.write(f"Deleted email {message.Subject} ({i:,})")
 
 
 if __name__ == "__main__":
